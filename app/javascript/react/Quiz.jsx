0f2f63fc3ab6ce0df5df7a95ecee1dd5c49489e2
Michael Milewski <saramic@gmail.com>
2019-07-05 18:57:58 +1000

    quick local variable work around for state of voting

--- app/javascript/react/Quiz.jsx
+++ app/javascript/react/Quiz.jsx
@@ -41,52 +41,60 @@ const VOTE_ON_QUESTION = gql`
   }
 `;
 
-const onVoteClick = ({ voteOnQuestion, event }) => {
-  event.preventDefault();
-  voteOnQuestion({
-    variables: {
-      quizId: event.target.dataset.quizId,
-      questionId: event.target.dataset.questionId,
-      status: parseInt(event.target.dataset.status, 10) // should use event.target.value?
-    }
-  });
-};
+const Question = ({ quizId, question, questionsQuiz }) => {
+  let voteStatus =
+    questionsQuiz &&
+    questionsQuiz.votes[0] &&
+    parseInt(questionsQuiz.votes[0].status, 10);
 
-const Question = ({ quizId, question, questionsQuiz }) => (
-  <li>
-    {question.text}
-    <Mutation mutation={VOTE_ON_QUESTION}>
-      {(voteOnQuestion, { loading, error }) => (
-        <>
-          {questionsQuiz ? (
-            <span>{questionsQuiz.votes[0].status === 0 ? "👍" : "👎"}</span>
-          ) : (
-            <>
-              <a
-                data-quiz-id={quizId}
-                data-question-id={question.id}
-                data-status={0}
-                onClick={event => onVoteClick({ voteOnQuestion, event })}
-              >
-                👍
-              </a>
-              <a
-                data-quiz-id={quizId}
-                data-question-id={question.id}
-                data-status={1}
-                onClick={event => onVoteClick({ voteOnQuestion, event })}
-              >
-                👎
-              </a>
-              {loading && <p>Loading...</p>}
-              {error && <p>Error :( Please try again</p>}
-            </>
-          )}
-        </>
-      )}
-    </Mutation>
-  </li>
-);
+  const onVoteClick = ({ voteOnQuestion, event }) => {
+    event.preventDefault();
+    voteStatus = parseInt(event.target.dataset.status, 10);
+    voteOnQuestion({
+      variables: {
+        quizId: event.target.dataset.quizId,
+        questionId: event.target.dataset.questionId,
+        status: voteStatus // should use event.target.value?
+      }
+    });
+  };
+
+  return (
+    <li>
+      {question.text}
+      <Mutation mutation={VOTE_ON_QUESTION}>
+        {(voteOnQuestion, { loading, error }) => (
+          <>
+            {typeof voteStatus != "undefined" ? (
+              <>{voteStatus === 0 ? "👍" : "👎"}</>
+            ) : (
+              <>
+                <a
+                  data-quiz-id={quizId}
+                  data-question-id={question.id}
+                  data-status={0}
+                  onClick={event => onVoteClick({ voteOnQuestion, event })}
+                >
+                  👍
+                </a>
+                <a
+                  data-quiz-id={quizId}
+                  data-question-id={question.id}
+                  data-status={1}
+                  onClick={event => onVoteClick({ voteOnQuestion, event })}
+                >
+                  👎
+                </a>
+                {loading && <p>Loading...</p>}
+                {error && <p>Error :( Please try again</p>}
+              </>
+            )}
+          </>
+        )}
+      </Mutation>
+    </li>
+  );
+};
 
 const Quiz = ({ id }) => {
   return (
